9da67d083001fa04a4225213c1ba3de197812e9a
Sofiia <sychak.pn@ucu.edu.ua>
2025-04-04 19:08:31 +0300

    Implements the solution for first task.

---
+++ Convert a linked list to a string/solution.py
@@ -0,0 +1,24 @@
+class Node:
+    """
+    Class of given linked list.
+    """
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def stringify(node):
+    """
+    Solution implementation.
+    """
+    current_node = node
+    next_node = current_node.next
+    list_of_data = []
+
+    while next_node != None:
+        list_of_data.append(current_node.data)
+        current_node = next_node
+        next_node = current_node.next
+    list_of_data.append(current_node.data)
+    list_of_data.append(None)
+
+    return " -> ".join(map(str, list_of_data))
