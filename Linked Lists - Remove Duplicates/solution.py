51e5c92108a95f309c4e992dc0f7b124d7bed9a4
Sofiia <sychak.pn@ucu.edu.ua>
2025-04-04 20:35:49 +0300

    Implementation of the solution and screens.

---
+++ Linked Lists - Remove Duplicates/solution.py
@@ -0,0 +1,17 @@
+def remove_duplicates(head):
+    if head is None:
+        return None
+
+    current_node = head
+
+    while current_node != None:
+        if current_node.next != None:
+            if current_node.data == current_node.next.data:
+                next_next_node = current_node.next.next
+                current_node.next = next_next_node
+            else:
+                current_node = current_node.next
+        else:
+            current_node = current_node.next
+
+    return head
