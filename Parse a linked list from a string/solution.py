ec7624a2842b9e872146deb4c97e5868a9ba297e
Sofiia <sychak.pn@ucu.edu.ua>
2025-04-04 19:13:52 +0300

    Implements the solution.

---
+++ Parse a linked list from a string/solution.py
@@ -0,0 +1,23 @@
+class Node:
+    """
+    Class of given linked list.
+    """
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(s):
+    """
+    Solution implementation.
+    """
+    if s == "None" or s == "null":
+        return None
+
+    splitted_string = s.split(" -> ")
+    node = None
+    for i, node_data in enumerate(splitted_string[:-1][::-1]):
+        if i == 0:
+            node = Node(int(node_data), None)
+        else:
+            node = Node(int(node_data), node)
+    return node
